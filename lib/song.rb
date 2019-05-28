56480df588f0ca73d361015787860b8fdf58883b
Kateryna Strough <katerynastrough@hotmail.com>
2019-05-28 23:08:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,3 +11,13 @@ def Song
   
   
 end
+
+ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "rap")
+ 
+ninety_nine_problems.name
+  # => "99 Problems"
+ 
+ninety_nine_problems.artist
+  # => "Jay-Z"
+ 
+ninety_nine_problems.genre
